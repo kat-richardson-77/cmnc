dc9630af7e77d12fbb36ed3c2401d479f4fedb5a
kat-richardson-77 <catykins@gmail.com>
2026-07-23 23:48:21 +0100

    Addition of pictures into files and onto Gallery Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,12 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("comSlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
